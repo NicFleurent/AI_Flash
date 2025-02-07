23134520678717a384af3fcdcd271f3fa66065ef
NicFleurent <fleurent.nicolas@hotmail.com>
2025-02-07 12:55:13 -0500

    Configuration de la navigation

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,12 +1,35 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStaticNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Intro from './pages/Intro';
 
 export default function App() {
+  const RootStack = createNativeStackNavigator({
+    initialRouteName: 'Intro',
+    screenOptions: {
+      headerStyle: {
+        backgroundColor: '#171717',
+      },
+      headerTintColor: '#1DB954',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+    screens: {
+      Intro: {
+        screen: Intro,
+        options: {
+          headerShown: false,
+        },
+      },
+    },
+  });
+  
+  const Navigation = createStaticNavigation(RootStack);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation/>
   );
 }
 
