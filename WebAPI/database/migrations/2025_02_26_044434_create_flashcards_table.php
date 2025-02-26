4c1eb80961563a3a299afba7cd14b08c6e0b14e8
Esdras Manefa <52791318+Manefa@users.noreply.github.com>
2025-02-26 08:32:38 -0500

    Creation migration flashcards

---
+++ WebAPI/database/migrations/2025_02_26_044434_create_flashcards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flashcards', function (Blueprint $table) {
+            $table->id();
+            $table->string('front_face');
+            $table->string('back_face');
+            $table->date('next_revision_date');
+            $table->integer('forgetting_curve_stage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flashcards');
+    }
+};
