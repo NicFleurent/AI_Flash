7b594f748e6e1b9b372277d3246294f108ed1509
Esdras Manefa <52791318+Manefa@users.noreply.github.com>
2025-03-09 01:46:10 -0500

    clean code

--- MobileApp/pages/collections/AddCollectionByMyself.js
+++ MobileApp/pages/collections/AddCollectionByMyself.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef, useState } from 'react';
-import { View, Text, StyleSheet, useWindowDimensions, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, useWindowDimensions, ActivityIndicator, Alert } from 'react-native'; // Ajouter Alert
 import CustomInput from '../../components/CustomInput';
 import AddFlashCardBottomSheet from '../../components/collections_components/bottoms_sheets/AddFlashCardBottomSheet';
 import CustomButton from '../../components/CustomButton';
@@ -11,20 +11,19 @@ import Toast from 'react-native-toast-message';
 import { useTranslation } from 'react-i18next';
 import { useNavigation } from '@react-navigation/native';
 
-
 const AddCollectionByMyself = ({ route }) => {
     const { width, height } = useWindowDimensions();
-
     const { id } = route.params || {};
     const [collectionName, setCollectionName] = useState("");
     const [erreurs, setErreurs] = useState({});
     const addFlashcardRef = useRef(null);
     const [flashCards, setFlashCards] = useState([]);
-    const [editingCard, setEditingCard] = useState(null); 
+    const [editingCard, setEditingCard] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const navigation = useNavigation();
 
+    // Fonction pour valider le formulaire
     const validerFormulaire = () => {
         const nouvellesErreurs = {};
         if (!collectionName.trim()) nouvellesErreurs.collectionName = t('add_collection_by_myself.collection_name_required');
@@ -33,7 +32,7 @@ const AddCollectionByMyself = ({ route }) => {
     };
 
     const handleAddFlashCard = (card) => {
-        setFlashCards([...flashCards, card]); 
+        setFlashCards([...flashCards, card]);
     };
 
     const handleEditFlashCard = (updatedCard) => {
@@ -44,27 +43,50 @@ const AddCollectionByMyself = ({ route }) => {
         );
     };
 
+    const handleDeleteFlashCard = (id) => {
+        Alert.alert(
+            t('flashcards.delete_confirmation.title'), 
+            t('flashcards.delete_confirmation.message'), 
+            [
+                {
+                    text: t('button.cancel'), 
+                    style: 'cancel',
+                },
+                {
+                    text: t('button.delete'), 
+                    onPress: () => {
+                        setFlashCards((prevCards) => prevCards.filter((card) => card.id !== id));
+                        Toast.show({
+                            type: 'success',
+                            text1: t('SUCCESS'),
+                            text2: t('flashcards.delete_success'), 
+                        });
+                    },
+                },
+            ],
+            { cancelable: true } 
+        );
+    };
+
     const handleSaveCollection = () => {
-        if(validerFormulaire()){
+        if (validerFormulaire()) {
             createCollectionAndFlashcards(id, collectionName, flashCards);
-        } 
+        }
     };
 
     const createCollectionAndFlashcards = async (subjectId, collectionName, flashcardsData) => {
-        setIsLoading(true); 
-      
-        try {
-          
-          const collectionResponse = await createCollection(subjectId, collectionName);
-      
-            if (collectionResponse && collectionResponse.data) {
+        setIsLoading(true);
 
+        try {
+            const collectionResponse = await createCollection(subjectId, collectionName);
+
+            if (collectionResponse && collectionResponse.data) {
                 const collectionId = collectionResponse.data.id;
 
                 for (const flashcard of flashcardsData) {
                     await createFlashcard({ ...flashcard, collection_id: collectionId });
                 }
-        
+
                 Toast.show({
                     type: 'success',
                     text1: t('SUCCESS'),
@@ -73,31 +95,28 @@ const AddCollectionByMyself = ({ route }) => {
 
                 navigation.navigate("Subjects");
             }
-
         } catch (error) {
-          console.log('Error: ' + error);
-      
-          Toast.show({
-            type: 'error',
-            text1: t('ERROR'),
-            text2: error.message,
-          });
-        } finally {
-            
-          setIsLoading(false); 
-        }
-      };
+            console.log('Error: ' + error);
 
-    const openBottomSheet = useCallback((card = null) => {    
-        setEditingCard(card); 
+            Toast.show({
+                type: 'error',
+                text1: t('ERROR'),
+                text2: error.message,
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const openBottomSheet = useCallback((card = null) => {
+        setEditingCard(card);
         addFlashcardRef.current?.expand();
     }, []);
 
     const closeBottomSheet = useCallback(() => {
-        setEditingCard(null); 
+        setEditingCard(null);
         addFlashcardRef.current?.close();
     }, []);
-
 
     const renderItem = ({ item }) => {
         return (
@@ -105,18 +124,18 @@ const AddCollectionByMyself = ({ route }) => {
                 title={item.front_face}
                 description={item.back_face}
                 isEditable={true}
-                handleEdit={() => openBottomSheet(item)} 
+                handleDelete={() => handleDeleteFlashCard(item.id)} 
+                handleEdit={() => openBottomSheet(item)}
             />
         );
-    };       
-    
+    };
+
     return (
-        
-        <GestureHandlerRootView style={{flex: 1}}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             <View style={styles.container}>
-                <View style={{marginTop: 20}}>
+                <View style={{ marginTop: 20 }}>
                     <CustomInput
-                        label= {t('add_collection_by_myself.collection_name')}
+                        label={t('add_collection_by_myself.collection_name')}
                         value={collectionName}
                         onChangeText={setCollectionName}
                         isPassword={false}
@@ -128,13 +147,14 @@ const AddCollectionByMyself = ({ route }) => {
                         type="white-outline"
                         label={t('add_collection_by_myself.new_card')}
                         additionnalStyle={{ marginTop: 20 }}
-                        onPress={() => openBottomSheet()} 
+                        onPress={() => openBottomSheet()}
                     />
 
-                    <View style={{height : height * 0.5, marginTop: 20}}>
-                        <FlatList 
-                            data={flashCards} 
+                    <View style={{ height: height * 0.5, marginTop: 20 }}>
+                        <FlatList
+                            data={flashCards}
                             renderItem={renderItem}
+                            keyExtractor={(item) => item.id.toString()} 
                         />
                     </View>
                 </View>
@@ -153,12 +173,14 @@ const AddCollectionByMyself = ({ route }) => {
                     collectionId={id}
                     initialData={editingCard}
                 />
-                
-                { isLoading &&
-                 <ActivityIndicator 
-                    style = {styles.overlay} 
-                    size="large" 
-                    color="#1DB954" />}            
+
+                {isLoading && (
+                    <ActivityIndicator
+                        style={styles.overlay}
+                        size="large"
+                        color="#1DB954"
+                    />
+                )}
             </View>
         </GestureHandlerRootView>
     );
@@ -169,23 +191,23 @@ const styles = StyleSheet.create({
         backgroundColor: "#000000",
         paddingHorizontal: 10,
         flex: 1,
-        justifyContent:"space-between"
+        justifyContent: "space-between",
     },
     errorText: {
         color: "red",
         fontSize: 12,
         marginBottom: 8,
     },
-    overlay:{
+    overlay: {
         position: 'absolute',
-        top: 0, 
+        top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.8)', 
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
 });
 
 export default AddCollectionByMyself;
